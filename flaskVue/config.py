778b0d10b1a587c5ac6d029ca0c8c76d58ef6f2b
Cheng-haixun <505029332@qq.com>
2019-10-25 13:52:09 +0800

    vueflask

--- flaskVue/config.py
+++ flaskVue/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     #连接数据库的信息,# url的格式为：数据库的协议：//用户名：密码@ip地址：端口号（默认可以不写）/数据库名
-    QLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@47.97.25.111:3306/flaskVue"
+    QLALCHEMY_DATABASE_URI = "mysql+pymysql://root:123456@47.97.25.111:3306/vueflask"
     # 动态追踪数据库的修改.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 会打印原生sql语句，便于观察测试
